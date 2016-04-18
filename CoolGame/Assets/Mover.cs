a8997cbeb0d122a883c9dbc3fb26b93af4533ac2
Mike Teixeira <mat8885@ad.rit.edu>
2016-04-18 14:43:41 -0400

    Committing Unity project
    So far we have a sprite that "jumps" with Space. I need to add platforms
    with collision as the next step.
    
    I'm thinking the player will be stationary, and everything else will
    move.

---
+++ CoolGame/Assets/Mover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    Vector3 acceleration;
+    Vector3 velocity;
+    Transform myTransform;
+    [SerializeField]
+    float maxMoveSpeed;
+    [SerializeField]
+    float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTransform.position += velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime;
+        if (myTransform.position.y < 0)
+        {
+            myTransform.position = new Vector3(myTransform.position.x, 0, myTransform.position.z);
+            velocity.y = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            velocity.y = jumpHeight;
+	}
+
+    public void SetVelocity(Vector3 direction, float percent)
+    {
+        velocity = direction.normalized * percent * maxMoveSpeed;
+    }
+
+    public void SetAcceleration(Vector3 accel)
+    {
+        acceleration = accel;
+    }
+}
---
+++ CoolGame/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    Transform myTransform;
+    Mover myMover;
+	// Use this for initialization
+	void Start () {
+        myTransform = transform;
+        myMover = (Mover)gameObject.GetComponent(typeof(Mover));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myMover.SetAcceleration(Vector3.down*20);
+
+        Camera.main.transform.position = new Vector3(myTransform.position.x, 1, myTransform.position.z - 2);
+	}
+}
