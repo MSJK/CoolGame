14ec74122855518d8a0ec64a93b002077664a417
JarellP4 <jarellp4@yahoo.com>
2016-04-25 14:52:49 -0400

    Incorporated Background Assets
    Added scripts, anims, and sprites into current scene. Tweaked game
    object values to fit with current game.

---
+++ CoolGame/Assets/Scripts/DigitGroup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DigitGroup : MonoBehaviour {
+
+    //GlitchVar
+    public bool isGlitching = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ CoolGame/Assets/Scripts/PrefabSpawn.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabSpawn : MonoBehaviour {
+
+    //bgAssets Object
+    private GameObject bgGroup;
+
+    //Prefab Slot
+    public GameObject prefabObj;
+    public float spawnInterval;
+    public float depthLevel;
+
+    //ScreenBoundValues
+    public float boundLeft;
+    public float boundRight;
+    public float boundTop;
+    public float boundBottom;
+
+	// Use this for initialization
+	void Start () {
+        bgGroup = this.transform.parent.gameObject;
+
+        StartCoroutine(InstancePrefab());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator InstancePrefab()
+    {
+        for(;;)
+        {
+            GameObject instance = Instantiate(prefabObj);
+
+            instance.transform.position = new Vector3 (boundRight, Random.Range(boundTop, boundBottom), depthLevel);
+            instance.transform.parent = this.transform;
+            instance.GetComponent<ScreenCull>().cullBound = boundLeft;
+            if(bgGroup.GetComponent<DigitGroup>().isGlitching)
+            {
+                instance.GetComponent<Animator>().enabled = true;
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
