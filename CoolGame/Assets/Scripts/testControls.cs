5039e482e8e98f77d32f2728fd37cad405dc9bcd
kolelp2 <kolelp2@yahoo.com>
2016-05-05 17:27:10 -0400

    Imported jump anims
    Put in anims for jump and fall with a properly working state machine.
    Just need to drop two lines of code in the player script to make the
    animation transitions trigger in game

---
+++ CoolGame/Assets/Scripts/testControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+            { this.GetComponent<Animator>().SetTrigger("JumpStart"); }
+
+        if (Input.GetButtonUp("Fire1"))
+        { this.GetComponent<Animator>().SetTrigger("FallStart"); }
+
+    }
+}
